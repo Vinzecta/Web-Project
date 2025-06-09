58251e8f0bcb3b860445bec978ba1aa8c5f47af0
Lmao1232j <vinhtran23042004@gmail.com>
2025-06-09 11:31:49 +0700

    Finish footer layout

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,8 +1,13 @@
+import Footer_Navigation from "./Footer_Navigation";
+
 function Footer() {
     return (
-        <footer className="flex justify-between border-t border-solid border-black p-5">
-            <p className="text-2xl ml-20">&copy; Copyrights</p>
-            <p className="text-2xl mr-20">All rights reserved</p>
+        <footer>
+            <Footer_Navigation/>
+            <section className="flex justify-between border-t border-solid border-black p-5">
+                <p className="text-2xl ml-20">&copy; Copyrights</p>
+                <p className="text-2xl mr-20">All rights reserved</p>
+            </section>
         </footer>
     );
 }
---
+++ src/footer/Footer_Navigation.jsx
@@ -0,0 +1,29 @@
+import Menu from "../assets/menu.svg"
+import Facebook from "../assets/facebook.svg"
+import Twitter from "../assets/twitter.svg"
+import Instagram from "../assets/instagram.svg"
+import Youtube from "../assets/youtube.svg"
+
+function Footer_Navigation() {
+    return (
+            <section className="flex justify-between p-5">
+                <h1 className="text-2xl ml-20 p-5">ShopName</h1>
+    
+                <nav className="flex gap-15 p-5">
+                    <a href="#" className="text-2xl m-auto">Home</a>
+                    <a href='#' className="text-2xl m-auto">About</a>
+                    <a href='#' className="text-2xl m-auto">Shop</a>
+                    <a href='#' className="text-2xl m-auto">Contact</a>
+                </nav>
+    
+                <div className="flex gap-5 mr-20">
+                    <img src={Facebook} alt="Facebook" className="w-10"/>
+                    <img src={Twitter} alt="Twitter" className="w-10"/>
+                    <img src={Instagram} alt="Instagram" className="w-10"/>
+                    <img src={Youtube} alt="Youtube" className="w-10"/>
+                </div>
+            </section>
+        );
+}
+
+export default Footer_Navigation;
