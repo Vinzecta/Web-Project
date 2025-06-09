1c8ef1a0dae727b32943cc86e3972d6a33ccc278
Lmao1232j <vinhtran23042004@gmail.com>
2025-06-09 11:01:14 +0700

    Finish footer layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './header/Header'
+import Footer from './footer/footer'
 
 function App() {
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   )
 }
---
+++ src/footer/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer className="flex justify-between border-t border-solid border-black p-5">
+            <p className="text-2xl ml-20">&copy; Copyrights</p>
+            <p className="text-2xl mr-20">All rights reserved</p>
+        </footer>
+    );
+}
+
+export default Footer;
