3d5e018c07b0ef7a725ffa11f62276840c730a32
Lmao1232j <vinhtran23042004@gmail.com>
2025-06-10 14:33:16 +0700

    Finish Shop Page Content

---
+++ src/Components/Shop/Shop.jsx
@@ -0,0 +1,89 @@
+import Header from "../header/Header";
+import Footer from "../footer/footer";
+import Hero from "../Hero";
+import Image1 from "../../assets/image-1.jpg"
+import Search from "../../assets/search.svg"
+import Rates from "../Rates"
+import Image7 from "../../assets/image-7.jpg"
+
+function Shop() {
+    return (
+        <>
+            <Header />
+
+            <section>
+                <div>
+                    <h2>Shop</h2>
+                    <div>
+                        <img src={Search} alt="Search Icon" />
+                        <input type='text' className="border"/>
+                    </div>
+                </div>
+
+                <div>
+                    <p>Showing all 11 results</p>
+                    <select className="border">
+                        <option>Product Name (A-Z)</option>
+                        <option>Product Name (Z-A)</option>
+                        <option>Released Date (Ascending)</option>
+                        <option>Released Date (Descending)</option>
+                    </select>
+                </div>
+            </section>
+
+            <section>
+                <div>
+                    <img src={Image7} alt="Image 7"/>
+                    <p>Ceramic</p>
+                    <h4>Ceramic Cup</h4>
+                    <Rates/>
+                    <p><span className="line-through">$100</span> $99</p>
+                </div>
+
+                <div>
+                    <img src={Image7} alt="Image 7"/>
+                    <p>Ceramic</p>
+                    <h4>Ceramic Cup</h4>
+                    <Rates/>
+                    <p><span className="line-through">$100</span> $99</p>
+                </div>
+
+                <div>
+                    <img src={Image7} alt="Image 7"/>
+                    <p>Ceramic</p>
+                    <h4>Ceramic Cup</h4>
+                    <Rates/>
+                    <p><span className="line-through">$100</span> $99</p>
+                </div>
+
+                <div>
+                    <img src={Image7} alt="Image 7"/>
+                    <p>Ceramic</p>
+                    <h4>Ceramic Cup</h4>
+                    <Rates/>
+                    <p><span className="line-through">$100</span> $99</p>
+                </div>
+
+                <div>
+                    <img src={Image7} alt="Image 7"/>
+                    <p>Ceramic</p>
+                    <h4>Ceramic Cup</h4>
+                    <Rates/>
+                    <p><span className="line-through">$100</span> $99</p>
+                </div>
+
+                <div>
+                    <img src={Image7} alt="Image 7"/>
+                    <p>Ceramic</p>
+                    <h4>Ceramic Cup</h4>
+                    <Rates/>
+                    <p><span className="line-through">$100</span> $99</p>
+                </div>
+            </section>
+
+            <Footer />
+        </>
+    );
+}
+
+export default Shop;
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import App from './App.jsx'
 import About from './Components/About Us/About.jsx'
 import Contact from './Components/Contact Us/Contact.jsx'
 import Homepage from './Components/Homepage/Homepage.jsx'
+import Shop from './Components/Shop/Shop.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Homepage />
+    <Shop />
   </StrictMode>,
 )
