5face2525e8743cf1a2025f990b91e680192e218
Lmao1232j <vinhtran23042004@gmail.com>
2025-06-12 16:17:04 +0700

    Finish styling Footer Component

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,10 +1,11 @@
 import Footer_Navigation from "./Footer_Navigation";
+import "./Footer.css"
 
 function Footer() {
     return (
         <footer>
             <Footer_Navigation/>
-            <section className="flex justify-between p-10 border-t bg-[#F1F4F1]">
+            <section className="flex justify-between p-10 border-t bg-[#F1F4F1]" id="copy-rights">
                 <p className="text-base ml-35">&copy; Copyrights</p>
                 <p className="text-base mr-35">All rights reserved</p>
             </section>
--- src/Components/footer/Footer_Navigation.jsx
+++ src/Components/footer/Footer_Navigation.jsx
@@ -6,17 +6,17 @@ import Youtube from "../../assets/youtube.svg"
 
 function Footer_Navigation() {
     return (
-            <section className="flex justify-around py-10 bg-[#F1F4F1]">
+            <section className="flex justify-around py-10 bg-[#F1F4F1]" id="footer-nav">
                 <h1 className="text-xl">ShopName</h1>
     
-                <nav className="flex gap-10">
+                <nav className="flex gap-10" id="footer-navi">
                     <a href="#" className="text-base">Home</a>
                     <a href='#' className="text-base">About</a>
                     <a href='#' className="text-base">Shop</a>
                     <a href='#' className="text-base">Contact</a>
                 </nav>
     
-                <div className="flex gap-2">
+                <div className="flex gap-2" id="social-media">
                     <img src={Facebook} alt="Facebook" className="w-5"/>
                     <img src={Twitter} alt="Twitter" className="w-5"/>
                     <img src={Instagram} alt="Instagram" className="w-5"/>
