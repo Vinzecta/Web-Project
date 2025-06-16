77248c5d1a780a07ffd4b5718547bb46287b19c1
Lmao1232j <vinhtran23042004@gmail.com>
2025-06-16 10:39:55 +0700

    Finish category (homepage) style

--- src/Components/Homepage/Category.jsx
+++ src/Components/Homepage/Category.jsx
@@ -6,28 +6,30 @@ function Category() {
             <div className="flex flex-col gap-5 w-1/2">
                 <p className="text-sm">THE BEST OR CERMAICS</p>
                 <h2 className="text-5xl">Our Products Category​</h2>
-                <p className="text-base">Explore our exquisite collection of ceramic treasures that elevate your spaces and celebrate the artistry of craftsmanship. From dinnerware to decor, each piece in our product range is a testament to quality, style, and the timeless beauty of ceramics.</p>
-                <div className="">
-                    <img src={Image5} alt="Image 5"/>
-                    <div className="">
-                        <p className="">Stylish Ceramic Home Decor</p>
-                        <p className="">Starting from just $99.00</p>
+                <p className="text-base w-[80%]">Explore our exquisite collection of ceramic treasures that elevate your spaces and celebrate the artistry of craftsmanship. From dinnerware to decor, each piece in our product range is a testament to quality, style, and the timeless beauty of ceramics.</p>
+                <div className="relative">
+                    <img src={Image5} alt="Image 5" className="w-1/1"/>
+                    <div className="bg-white w-[90%] absolute bottom-[5%] left-1/2 transform -translate-x-1/2 p-5">
+                        <p className="text-2xl">Stylish Ceramic Home Decor</p>
+                        <p className="text-base">Starting from just $99.00</p>
                     </div>
                 </div>
             </div>
 
             <div className="flex flex-col gap-5 w-1/2 ">
-                <div style={{ backgroundImage: `url(${Image5})` }} className="bg-cover bg-center h-full w-full">
-                    <div>
-                        <p className="text-2xl">Product A</p>
-                        <p className="text-base">Start from $1</p>
+                <div className="relative">
+                    <img src={Image5} alt="Image 5" className="w-1/1"/>
+                    <div className="bg-white w-[90%] absolute bottom-[5%] left-1/2 transform -translate-x-1/2 p-5">
+                        <p className="text-2xl">Stylish Ceramic Home Decor</p>
+                        <p className="text-base">Starting from just $99.00</p>
                     </div>
                 </div>
 
-                <div>
-                    <div style={{ backgroundImage: `url(${Image5})` }} className="bg-cover bg-center h-full w-full">
-                        <p className="text-2xl">Product A</p>
-                        <p className="text-base">Start from $1</p>
+                <div className="relative">
+                    <img src={Image5} alt="Image 5" className="w-1/1"/>
+                    <div className="bg-white w-[90%] absolute bottom-[5%] left-1/2 transform -translate-x-1/2 p-5">
+                        <p className="text-2xl">Stylish Ceramic Home Decor</p>
+                        <p className="text-base">Starting from just $99.00</p>
                     </div>
                 </div>
             </div>
