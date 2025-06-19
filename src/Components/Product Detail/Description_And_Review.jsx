ae771e8a8fa1ee135a745236dcfdf7b850099da0
Lmao1232j <vinhtran23042004@gmail.com>
2025-06-19 16:02:22 +0700

    In progress description_and_review section

--- src/Components/Product Detail/Description_And_Review.jsx
+++ src/Components/Product Detail/Description_And_Review.jsx
@@ -1,89 +1,98 @@
 import Rates from "../Rates"
 import Avatar from "../../assets/avatar.jpg"
+import { useState } from "react";
 
 function Description_And_Review() {
+    const [change, setChange] = useState(false);
+
+    function changeToDescription() {
+        setChange(false);
+    }
+
+    function changeToReview() {
+        setChange(true);
+    }
+
+
     return (
-        <section>
-            <div>
-                <p>Description</p>
-                <p>Review</p>
+        <section className="w-[80%] mx-auto mt-10">
+            <div className="border-t flex gap-10">
+                <button className={`text-base ${!change ? 'border-t-3 border-t-[#C25C5C]' : ''}`} onClick={changeToDescription}>Description</button>
+                <button className={`text-base ${change ? 'border-t-3 border-t-[#C25C5C]' : ''}`} onClick={changeToReview}>Review</button>
             </div>
 
-            {/* Description */}
-            <div>
-                <p>Discover the beauty of ceramics with our handcrafted. This exquisite piece combines artistry and function, making it a versatile addition to any space. Whether used as a decorative accent or a practical item, this ceramic creation adds a touch of elegance to your surroundings.</p>
-            </div>
+            {!change ? <div className="mt-5">
+                            <p className="text-base">Discover the beauty of ceramics with our handcrafted. This exquisite piece combines artistry and function, making it a versatile addition to any space. Whether used as a decorative accent or a practical item, this ceramic creation adds a touch of elegance to your surroundings.</p>
+                       </div> : 
 
-            {/* Reviews */}
-            <div>
-                <div>
-                    <Rates />
-                    <p>Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                       <>
+                            <div className="mt-5 flex flex-col gap-5">
+                                <div className="flex gap-5 p-5 rounded-2xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+                                    <img src={Avatar} alt="Avatar" className="w-[60px] rounded-full"/>
 
-                    <div>
-                        <img src={Avatar} alt="Avatar"/>
-                        <p>User A</p>
-                    </div>
-                </div>
+                                    <div className="flex flex-col gap-2">
+                                        <Rates />
+                                        <p>This shit is so ass!</p>
+                                    </div>
+                                </div>
 
-                <div>
-                    <Rates />
-                    <p>Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                                <div className="flex gap-5 p-5 rounded-2xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+                                    <img src={Avatar} alt="Avatar" className="w-[60px] rounded-full"/>
 
-                    <div>
-                        <img src={Avatar} alt="Avatar"/>
-                        <p>User A</p>
-                    </div>
-                </div>
+                                    <div className="flex flex-col gap-2">
+                                        <Rates />
+                                        <p>This shit is so ass!</p>
+                                    </div>
+                                </div>
 
-                <div>
-                    <Rates />
-                    <p>Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                                <div className="flex gap-5 p-5 rounded-2xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+                                    <img src={Avatar} alt="Avatar" className="w-[60px] rounded-full"/>
 
-                    <div>
-                        <img src={Avatar} alt="Avatar"/>
-                        <p>User A</p>
-                    </div>
-                </div>
+                                    <div className="flex flex-col gap-2">
+                                        <Rates />
+                                        <p>This shit is so ass!</p>
+                                    </div>
+                                </div>
 
-                <div>
-                    <Rates />
-                    <p>Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                                <div className="flex gap-5 p-5 rounded-2xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+                                    <img src={Avatar} alt="Avatar" className="w-[60px] rounded-full"/>
 
-                    <div>
-                        <img src={Avatar} alt="Avatar"/>
-                        <p>User A</p>
-                    </div>
-                </div>
-                <button className="border">Load More</button>
-            </div>
+                                    <div className="flex flex-col gap-2">
+                                        <Rates />
+                                        <p>This shit is so ass!</p>
+                                    </div>
+                                </div>
 
-            {/* Your Reviews */}
-            <div>
-                <h2>Your Review</h2>
-                <p>We truly appreciate your review — it helps us make our products even better for you.</p>
+                                <button type="submit" className="border text-base w-[10%] py-3 border-[#C25C5C] text-[#C25C5C] rounded-3xl mx-auto" id="submit">Load More</button>
+                            </div>
+                            
+                            <form className="mt-10 p-10 flex flex-col gap-5 border border-gray-300 border-opacity-50">
+                                <h2 className="text-xl">Your Review</h2>
+                                <p className="text-base">We truly appreciate your review — it helps us make our products even better for you.</p>
 
-                {/* Ratings */}
-                <div>
-                    <label>Your Ratings</label>
-                    <Rates />
-                </div>
+                                <div className="flex gap-5">
+                                    <label className="text-xl">Your Ratings <span className="text-[red]">*</span></label>
+                                    <Rates />
+                                </div>
 
-                <label>Your Review</label>
-                <textarea type="text" className="border" />
+                                <label className="text-xl">Your Review <span className="text-[red]">*</span></label>
+                                <textarea type="text" className="bg-[white] h-[120px] px-3 py-3 border border-gray-300 border-opacity-50" />
 
-                <div>
-                    <div>
-                        <label>Name</label>
-                        <input type="text" className="border"></input>
-                    </div>
+                                <div className="flex gap-5">
+                                    <div className="flex flex-col gap-5 w-[50%]">
+                                        <label className="text-base">Name <span className="text-[red]">*</span></label>
+                                        <input type="text" className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50"></input>
+                                    </div>
 
-                    <div>
-                        <label>Email</label>
-                        <input type="text" className="border"></input>
-                    </div>
-                </div>
-            </div>
+                                    <div className="flex flex-col gap-5 w-[50%]">
+                                        <label className="text-base">Email <span className="text-[red]">*</span></label>
+                                        <input type="text" className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50"></input>
+                                    </div>
+                                </div>
+
+                                <button type="submit" className="border text-base w-[10%] py-3 border-[#C25C5C] text-[#C25C5C]" id="submit">SUBMIT</button>
+                            </form>
+                        </>}
         </section>
     );
 }
