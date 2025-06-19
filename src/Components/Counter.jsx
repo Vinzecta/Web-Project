dc324d126e2b7ed94ebb37599e943e13080a5340
Lmao1232j <vinhtran23042004@gmail.com>
2025-06-19 11:36:08 +0700

    Finish Product Detail Section (Detail page)

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,12 +1,12 @@
 import Plus from "../assets/plus.svg"
 import Minus from "../assets/minus.svg"
 
-function Counter() {
+function Counter({...props}) {
     return (
-        <div className="flex">
-            <img src={Minus} alt="Minus Icon" className="w-[30px] border"/>
-            <p className="px-[5%] h-[30px] border text-base">1</p>
-            <img src={Plus} alt="Plus Icon" className="w-[30px] border"/>
+        <div {...props}>
+            <img src={Minus} alt="Minus Icon" className="w-[30px] h-[30px] border"/>
+            <p className="px-[30px] h-[30px] border text-base">1</p>
+            <img src={Plus} alt="Plus Icon" className="w-[30px] h-[30px] border"/>
         </div>
     );
 }
--- src/Components/Product Detail/Product_Detail.jsx
+++ src/Components/Product Detail/Product_Detail.jsx
@@ -1,23 +1,31 @@
 import Image7 from "../../assets/image-7.jpg"
 import Counter from "../Counter";
+import "./Product_Detail.css"
 
 function Product_Detail() {
     return (
-        <section className="w-[80%] mx-auto mt-10 flex justify-between">
-            <div className="h-[20%] w-[50%]">
-                <img src={Image7} alt="Image 7" className="h-full w-full object-cover" />  
+        <section className="w-[80%] mx-auto mt-10 flex gap-10" id="detail-section">
+            <div className="w-[50%] flex gap-2" id="image-container">
+                <div className="flex flex-col gap-2 w-fit h-fit" id="small-image-container">
+                    <img src={Image7} alt="Image 7" className="w-[120px]" />
+                    <img src={Image7} alt="Image 7" className="w-[120px]" />
+                </div>
+
+                <img src={Image7} alt="Image 7" className="h-full w-full object-cover" />
             </div>
 
-            <div className="w-[40%] flex flex-col gap-5">
-                 <p className="text-sm">Home/Ceramic/Ceramic Pot</p>
-                 <h4 className="text-base">CERAMIC</h4>
-                 <h3 className="text-[32px]">Ceramic Plant Pot</h3>
-                 <p className="text-2xl"><span className="line-through">$100</span> $99</p>
-                 <p className="text-base">Discover the beauty of ceramics with our handcrafted. This exquisite piece combines artistry and function, making it a versatile addition to any space. Whether used as a decorative accent or a practical item, this ceramic creation adds a touch of elegance to your surroundings.</p>   
-                 <div>
-                     <Counter />
-                     <button className="border text-base">ADD TO CART</button>
+            <div className="w-[50%] flex flex-col gap-5" id="info-container">
+                 <p className="text-sm">Home / Ceramic / Ceramic Pot</p>
+                 <h4 className="text-base" id="product-category">CERAMIC, CERAMIC COATING, POTTERY</h4>
+                 <h3 className="text-[32px]" id="product-name">Ceramic Plant Pot</h3>
+                 <p className="text-2xl" id="price"><span className="line-through">$100</span> $99</p>
+                 <p className="text-base" id="description">Discover the beauty of ceramics with our handcrafted. This exquisite piece combines artistry and function, making it a versatile addition to any space. Whether used as a decorative accent or a practical item, this ceramic creation adds a touch of elegance to your surroundings.</p>   
+                 <div className="flex gap-10 pb-5 border-b" id="quantity">
+                     <Counter className="flex my-auto" />
+                     <button className="text-base text-[#C25C5C] w-fit py-[10px] px-[30px] border border-[#C25C5C] banner-p">ADD TO CART</button>
                  </div>
+
+                 <p className="text-base" id="category">Categories: <span>Ceramic, Ceramic Coating, Pottery</span></p>
             </div>
         </section>
     );
