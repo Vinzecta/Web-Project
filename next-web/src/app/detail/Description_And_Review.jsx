0e39b24a2605132889772e877a23cd906f993dcb
Lmao1232j <vinhtran23042004@gmail.com>
2025-06-23 14:41:45 +0700

    Finish styling product detail page

--- next-web/src/app/detail/Description_And_Review.jsx
+++ next-web/src/app/detail/Description_And_Review.jsx
@@ -19,18 +19,18 @@ function Description_And_Review() {
 
 
     return (
-        <section className="w-[80%] mx-auto mt-10">
-            <div className="border-t flex gap-10">
+        <section className="mt-10">
+            <div className="w-[80%] mx-auto border-t flex gap-10">
                 <button className={`text-base ${!change ? 'border-t-3 border-t-[#C25C5C]' : ''}`} onClick={changeToDescription} id="description">Description</button>
                 <button className={`text-base ${change ? 'border-t-3 border-t-[#C25C5C]' : ''}`} onClick={changeToReview} id="review">Review</button>
             </div>
 
-            {!change ? <div className="mt-5">
+            {!change ? <div className="w-[80%] mx-auto mt-5">
                             <p className="text-base" id="description-content">Discover the beauty of ceramics with our handcrafted. This exquisite piece combines artistry and function, making it a versatile addition to any space. Whether used as a decorative accent or a practical item, this ceramic creation adds a touch of elegance to your surroundings.</p>
                        </div> : 
 
                        <>
-                            <div className="mt-5 flex flex-col gap-5">
+                            <div className="w-[80%] mx-auto mt-5 flex flex-col gap-5">
                                 <div className="flex gap-5 p-5 rounded-2xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
                                     <Image src={Avatar} alt="Avatar" className="w-[60px] rounded-full avatar"/>
 
@@ -67,34 +67,34 @@ function Description_And_Review() {
                                     </div>
                                 </div>
 
-                                <button className="border text-base w-[10%] py-3 border-[#C25C5C] text-[#C25C5C] rounded-3xl mx-auto" id="load-more">Load More</button>
+                                <button className="border text-base w-fit py-3 px-5 border-[#C25C5C] text-[#C25C5C] rounded-3xl mx-auto" id="load-more">Load More</button>
                             </div>
                             
-                            <form className="mt-10 p-10 flex flex-col gap-5 border border-gray-300 border-opacity-50">
-                                <h2 className="text-xl">Your Review</h2>
-                                <p className="text-base">We truly appreciate your review — it helps us make our products even better for you.</p>
+                            <form className="w-[80%] mx-auto mt-10 p-10 flex flex-col gap-5 border border-gray-300 border-opacity-50" id="review-form">
+                                <h2 className="text-xl" id="your-review">Your Review</h2>
+                                <p className="text-base" id="review-content">We truly appreciate your review — it helps us make our products even better for you.</p>
 
                                 <div className="flex gap-5">
-                                    <label className="text-xl">Your Ratings <span className="text-[red]">*</span></label>
+                                    <label className="text-xl" id="your-rating">Your Ratings <span className="text-[red]">*</span></label>
                                     <Rates />
                                 </div>
 
-                                <label className="text-xl">Your Review <span className="text-[red]">*</span></label>
-                                <textarea type="text" className="bg-[white] h-[120px] px-3 py-3 border border-gray-300 border-opacity-50" />
+                                <label className="text-xl" id="your-review">Your Review <span className="text-[red]">*</span></label>
+                                <textarea type="text" className="bg-[white] h-[120px] px-3 py-3 border border-gray-300 border-opacity-50 label-text" id="review-enter" />
 
-                                <div className="flex gap-5">
-                                    <div className="flex flex-col gap-5 w-[50%]">
-                                        <label className="text-base">Name <span className="text-[red]">*</span></label>
+                                <div className="flex gap-5" id="name-info">
+                                    <div className="flex flex-col gap-5 w-[50%] name-infos">
+                                        <label className="text-base label-text">Name <span className="text-[red]">*</span></label>
                                         <input type="text" className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50"></input>
                                     </div>
 
-                                    <div className="flex flex-col gap-5 w-[50%]">
-                                        <label className="text-base">Email <span className="text-[red]">*</span></label>
+                                    <div className="flex flex-col gap-5 w-[50%] name-infos">
+                                        <label className="text-base label-text">Email <span className="text-[red]">*</span></label>
                                         <input type="text" className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50"></input>
                                     </div>
                                 </div>
 
-                                <button type="submit" className="border text-base w-[10%] py-3 border-[#C25C5C] text-[#C25C5C]" id="submit">SUBMIT</button>
+                                <button type="submit" className="border text-base w-fit py-3 px-5 border-[#C25C5C] text-[#C25C5C]" id="submit">SUBMIT</button>
                             </form>
                         </>}
         </section>
