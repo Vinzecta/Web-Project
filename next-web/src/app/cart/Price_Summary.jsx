d3065297db9d91a12a0baca78d2ced64fbc5e4ee
Lmao1232j <vinhtran23042004@gmail.com>
2025-06-25 11:53:10 +0700

    In progress

--- next-web/src/app/cart/Price_Summary.jsx
+++ next-web/src/app/cart/Price_Summary.jsx
@@ -6,63 +6,73 @@ import Paid from "../../../public/paid.svg"
 import User from "../../../public/user.svg"
 import Bill from "../../../public/bill.svg"
 import Card from "../../../public/card.svg"
-import Cash from "../../../public/cash.svg" 
-import Select from "../select/Select";  
+import Cash from "../../../public/cash.svg"  
+import Paypal from "../../../public/paypal.svg" 
 
 export default function Price_Summary() {
     return (
-        <section>
-            <div>
-                <Image src={Bill} alt="Paid Logo" />
-                <h2>Billing Detail</h2>
+        <section className="w-[80%] mt-10 mx-auto">
+            <div className="flex gap-5">
+                <Image src={Bill} className="w-[32px]" alt="Paid Logo" />
+                <h2 className="text-[32px]">Billing Detail</h2>
             </div>
 
-            <div>
-                <div>
+            <div className="p-5 flex flex-col gap-5">
+                <div className="flex gap-2">
                     <Image src={User} alt="User" />
-                    <h3>Customer Information</h3>
+                    <h3 className="text-[20px]">Customer Information</h3>
+                    <button className="ml-auto">Change</button>
                 </div>
 
-                <p>Name: <span>Vinzecta</span></p>
-                <p>Phone Number: <span>0366150204</span></p>
-                <p>Address: <span>Binh Tan District, HCMC</span></p>
+                <p className="text-[16px]">Name: <span>Vinzecta</span></p>
+                <p className="text-[16px]">Phone Number: <span>0366150204</span></p>
+                <p className="text-[16px]">Address: <span>Binh Tan District, HCMC</span></p>
             </div>
 
-            <div>
-                <div>
+            <div className="p-5 flex flex-col gap-5">
+                <div className="flex gap-2">
                     <Image src={Paid} alt="Paid Icon"/>
-                    <h3>Total Amount</h3>
+                    <h3 className="text-[20px]">Total Amount</h3>
                 </div>
 
-                <div>
-                    <p>Subtotal:</p>
-                    <p>$1000</p>
+                <p>Subtotal: <span>$1000</span></p>
+                <p>Shipment Cost (3km): <span>$1</span></p>
+
+                <div className="flex gap-5">
+                    <input placeholder="Enter the coupon" className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50"/>
+                    <button className="border text-base py-3 px-10 rounded-3xl border-[#C25C5C] text-[#C25C5C]">Apply</button>
                 </div>
 
-                <div>
-                    <p>Shipment Cost <span>(3km): </span></p>
-                    <p>$1</p>
-                </div>
-
-                <div>
-                    <input placeholder="Enter the coupon"/>
-                    <button>Apply</button>
-                </div>
-
-                <div>
-                    <p>Total: </p>
-                    <p>$1001</p>
-                </div>
+                <p>Total: <span>$1001</span></p>
             </div>
 
-            <div>
-                <div>
-                    <Image src={Card} alt="Card" />
-                    <h3>Payment Method</h3>
+            <div className="p-5 flex flex-col gap-5">
+                <div className="flex gap-2">
+                    <Image src={Card} alt="Card" className="w-[30px]" />
+                    <h3 className="text-[20px]">Payment Methods</h3>
                 </div>
 
-                <Select />
+                <div className="w-[50%] mx-auto flex flex-col gap-2">
+                    <button className="flex w-[100%] py-5 justify-center">
+                        <Image src={Cash} className="w-[30px]" alt="Cash Icon" /> 
+                        <p className="text-[16px]">Cash</p>
+                    </button>
+
+                    <button className="flex w-[100%] justify-center py-5">
+                        <Image src={Paypal} className="w-[30px]" alt="Paypal Icon" />
+                        <p className="text-[16px]">Paypal</p>
+                    </button>
+
+                    <button className="flex w-[100%] justify-center py-5">
+                        <img src="momo.svg" className="w-[30px]" alt="Momo Icon" />
+                        <p className="text-[16px]">Momo</p>
+                    </button>
+                </div>
+
+                <button>Add more payment method</button>
             </div>
+
+            <button>Place Order $1001</button>
         </section>
     );
 }
