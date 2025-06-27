ff63593aba05cbb7dee66eecdd8b2a3ee1bdce43
Lmao1232j <vinhtran23042004@gmail.com>
2025-06-27 10:37:46 +0700

    Finish profile section responsive

--- next-web/src/app/account/Profile.jsx
+++ next-web/src/app/account/Profile.jsx
@@ -3,16 +3,17 @@ import Avatar from "../../../public/avatar.jpg"
 import Image from "next/image";
 import Information from "../../../public/information.svg"
 import Address from "../../../public/address.svg"
+import "./Profile.css"
 
 function Profile() {
     return (
-        <form className="w-[60%] mt-5 p-5 flex flex-col gap-5 rounded-2xl border border-gray-300 border-opacity-50 mb-10">
-            <h2 className="text-[25px] pb-3 border-b">Profile Information</h2>
+        <form className="w-[60%] mt-5 p-5 flex flex-col gap-5 rounded-2xl border border-gray-300 border-opacity-50 mb-10" id="profile-form">
+            <h2 className="text-[25px] pb-3 border-b" id="profile-information">Profile Information</h2>
 
             {/* Avatar */}
             <div className="flex flex-col gap-5">
-                <Image src={Avatar} alt="Avatar" className="w-[120px] rounded-full mx-auto" />
-                <button className="py-3 w-fit px-5 mx-auto rounded-3xl border border-[#C25C5C] text-[#C25C5C]">Change Image</button>
+                <Image src={Avatar} alt="Avatar" className="w-[120px] rounded-full mx-auto" id="profile-image"/>
+                <button className="py-3 w-fit px-5 mx-auto rounded-3xl border border-[#C25C5C] text-[#C25C5C]" id="change-image">Change Image</button>
             </div>
 
             {/* Basic Information */}
@@ -22,25 +23,25 @@ function Profile() {
                     <h3 className="text-[20px]">Basic Information</h3>
                 </div>
 
-                <div className="flex justify-between">
-                    <div className="w-[45%] flex flex-col gap-2">
+                <div className="flex justify-between basic-info">
+                    <div className="w-[45%] flex flex-col gap-2 flex-info">
                         <label>First Name</label>
                         <input className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%]"></input>
                     </div>
 
-                    <div className="w-[45%] flex flex-col gap-2">
+                    <div className="w-[45%] flex flex-col gap-2 flex-info">
                         <label>Last Name</label>
                         <input className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%]"></input>
                     </div>
                 </div>
 
-                <div className="flex justify-between">
-                    <div className="w-[45%] flex flex-col gap-2">
+                <div className="flex justify-between basic-info">
+                    <div className="w-[45%] flex flex-col gap-2 flex-info">
                         <label>Username</label>
                         <input className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50"></input>
                     </div>
 
-                    <div className="w-[45%] flex flex-col gap-2">
+                    <div className="w-[45%] flex flex-col gap-2 flex-info">
                         <label>Email</label>
                         <input className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50"></input>
                     </div>
@@ -54,23 +55,23 @@ function Profile() {
                     <h3 className="text-[20px]">Address</h3>
                 </div>
 
-                <div className="flex justify-between">
-                    <div className="w-[20%] flex flex-col gap-2">
+                <div className="flex justify-between" id="address-info">
+                    <div className="w-[20%] flex flex-col gap-2 address-flex">
                         <label>City</label>
                         <select className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%]"></select>
                     </div>
 
-                    <div className="w-[20%] flex flex-col gap-2">
+                    <div className="w-[20%] flex flex-col gap-2 address-flex">
                         <label>Province</label>
                         <select className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%]"></select>
                     </div>
 
-                    <div className="w-[20%] flex flex-col gap-2">
+                    <div className="w-[20%] flex flex-col gap-2 address-flex">
                         <label>District</label>
                         <select className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%]"></select>
                     </div>
 
-                    <div className="w-[20%] flex flex-col gap-2">
+                    <div className="w-[20%] flex flex-col gap-2 address-flex">
                         <label>Ward</label>
                         <select className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%]"></select>
                     </div>
