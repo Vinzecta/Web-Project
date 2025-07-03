46780b5d8e90d99d339165dd34c5cb79f71c6faf
Lmao1232j <vinhtran23042004@gmail.com>
2025-07-03 12:09:16 +0700

    Finish menu content

--- next-web/src/app/Menu/Menu.jsx
+++ next-web/src/app/Menu/Menu.jsx
@@ -3,6 +3,10 @@ import Image from "next/image"
 import Avatar from "../../../public/avatar.jpg"
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import { Work_Sans } from "next/font/google";
+import Link from "next/link";
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
+import Image7 from "../../../public/image-7.jpg"
+import Counter from "../counter/Counter";
 
 const workSans = Work_Sans({
   subsets: ["latin"],
@@ -13,7 +17,7 @@ const workSans = Work_Sans({
 export default function MenuBar() {
     return (
         <section className="h-screen absolute bg-[#00000066] w-[100%] top-0 z-1">
-            <div className="h-[100%] w-[35%] bg-white ml-auto">
+            <div className="h-[100%] w-[35%] bg-white ml-auto flex flex-col">
                 <div className="w-[100%] flex justify-between py-3 px-5 border-b border-gray-300 border-opacity-50">
                     <div className="flex gap-5">
                         <Image src={Avatar} className="rounded-full w-[50px]" alt="Avatar" />
@@ -23,7 +27,202 @@ export default function MenuBar() {
                     <CloseCircleOutlined />
                 </div>
 
+                <div className="hidden">
+                    <p className={`${workSans.className} text-[#424b4a] text-normal text-center`}>Continue with your account or sign up now.</p>
+                    <div className="flex justify-around mt-5">
+                        <Link className={`text-base w-[30%] py-3 text-center border banner-p text-[#C25C5C] rounded-3xl border-[#C25C5C] font-medium ${workSans.className} hover:bg-[#C25C5C] hover:text-white delay-75 duration-300 flex justify-center items-center`} href="/registeration">Sign In</Link>
+                        <Link className={`text-base w-[30%] py-3 text-center border banner-p text-[#C25C5C] rounded-3xl border-[#C25C5C] font-medium ${workSans.className} hover:bg-[#C25C5C] hover:text-white delay-75 duration-300 flex justify-center items-center`} href="/registeration">Sign Up</Link>
+                    </div>
+                </div>
 
+                <div>
+                    <div className="w-[100%]">
+                        <div className="flex gap-3 justify-end px-5 py-3">
+                            <p className="text-[20px] font-medium text-[#424b4a]">Account</p>
+                            <AccountCircleOutlinedIcon className="my-auto text-[#424b4a]" />
+                        </div>
+
+                        <div className="flex gap-3 justify-end px-5 py-3">
+                            <p className="text-[20px] font-medium text-[#424b4a]">Sign Out</p>
+                            <PowerSettingsNewIcon className="my-auto text-[#424b4a]"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="w-[100%] border-t border-gray-300 border-opacity-50">
+                    <h2 className="mt-5 text-[25px] font-semibold text-[#C25C5C] text-center">Your Cart</h2>
+
+                    <div className="w-[100%] max-h-[332px] overflow-y-auto">
+                        <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
+                            <Image src={Image7} alt="Image7" className="w-[70px]" />
+
+                            <div className="w-[100%] flex flex-col gap-3">
+                                <div className="flex justify-between">
+                                    <p>Ceramic Pot</p>
+                                    <CloseCircleOutlined />
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <Counter className="flex" />
+                                    <p>$99</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
+                            <Image src={Image7} alt="Image7" className="w-[70px]" />
+
+                            <div className="w-[100%] flex flex-col gap-3">
+                                <div className="flex justify-between">
+                                    <p>Ceramic Pot</p>
+                                    <CloseCircleOutlined />
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <Counter className="flex" />
+                                    <p>$99</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
+                            <Image src={Image7} alt="Image7" className="w-[70px]" />
+
+                            <div className="w-[100%] flex flex-col gap-3">
+                                <div className="flex justify-between">
+                                    <p>Ceramic Pot</p>
+                                    <CloseCircleOutlined />
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <Counter className="flex" />
+                                    <p>$99</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
+                            <Image src={Image7} alt="Image7" className="w-[70px]" />
+
+                            <div className="w-[100%] flex flex-col gap-3">
+                                <div className="flex justify-between">
+                                    <p>Ceramic Pot</p>
+                                    <CloseCircleOutlined />
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <Counter className="flex" />
+                                    <p>$99</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
+                            <Image src={Image7} alt="Image7" className="w-[70px]" />
+
+                            <div className="w-[100%] flex flex-col gap-3">
+                                <div className="flex justify-between">
+                                    <p>Ceramic Pot</p>
+                                    <CloseCircleOutlined />
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <Counter className="flex" />
+                                    <p>$99</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
+                            <Image src={Image7} alt="Image7" className="w-[70px]" />
+
+                            <div className="w-[100%] flex flex-col gap-3">
+                                <div className="flex justify-between">
+                                    <p>Ceramic Pot</p>
+                                    <CloseCircleOutlined />
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <Counter className="flex" />
+                                    <p>$99</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
+                            <Image src={Image7} alt="Image7" className="w-[70px]" />
+
+                            <div className="w-[100%] flex flex-col gap-3">
+                                <div className="flex justify-between">
+                                    <p>Ceramic Pot</p>
+                                    <CloseCircleOutlined />
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <Counter className="flex" />
+                                    <p>$99</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
+                            <Image src={Image7} alt="Image7" className="w-[70px]" />
+
+                            <div className="w-[100%] flex flex-col gap-3">
+                                <div className="flex justify-between">
+                                    <p>Ceramic Pot</p>
+                                    <CloseCircleOutlined />
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <Counter className="flex" />
+                                    <p>$99</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
+                            <Image src={Image7} alt="Image7" className="w-[70px]" />
+
+                            <div className="w-[100%] flex flex-col gap-3">
+                                <div className="flex justify-between">
+                                    <p>Ceramic Pot</p>
+                                    <CloseCircleOutlined />
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <Counter className="flex" />
+                                    <p>$99</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
+                            <Image src={Image7} alt="Image7" className="w-[70px]" />
+
+                            <div className="w-[100%] flex flex-col gap-3">
+                                <div className="flex justify-between">
+                                    <p>Ceramic Pot</p>
+                                    <CloseCircleOutlined />
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <Counter className="flex" />
+                                    <p>$99</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-between py-3 px-5 border-t border-b border-gray-300 border-opacity-50">
+                        <p>Subtotal:</p>
+                        <p>$99</p>
+                    </div>
+                </div>
+
+                <div className="mt-10 px-3 w-[100%]">
+                    <Link href="/cart" className={`text-base w-[100%] py-3 px-5 text-center border banner-p text-[#C25C5C] m-auto font-medium ${workSans.className} hover:bg-[#C25C5C] hover:text-white delay-75 duration-300 flex justify-center items-center`}>VIEW CART</Link>
+                </div>
             </div>
         </section>
     )
