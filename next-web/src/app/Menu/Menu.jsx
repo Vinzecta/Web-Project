217e0e3668babf473086e6d5d39f3eeb989bf6ee
Lmao1232j <vinhtran23042004@gmail.com>
2025-07-04 11:51:26 +0700

    OK

--- next-web/src/app/Menu/Menu.jsx
+++ next-web/src/app/Menu/Menu.jsx
@@ -10,6 +10,10 @@ import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 import Image7 from "../../../public/image-7.jpg"
 import Counter from "../counter/Counter";
 import { useState, useEffect } from "react";
+import NavigationOutlinedIcon from '@mui/icons-material/NavigationOutlined';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import "./Menu.css"
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const workSans = Work_Sans({
   subsets: ["latin"],
@@ -19,6 +23,7 @@ const workSans = Work_Sans({
 
 export default function MenuBar({onClose, menuStatus}) {
     const [animationClass, setAnimationClass] = useState("translate-x-full");
+    const [openNav, setOpenNav] = useState(false);
 
     useEffect(() => {
         if (menuStatus) {
@@ -33,7 +38,7 @@ export default function MenuBar({onClose, menuStatus}) {
 
     return (
         <section className="h-screen w-[100%] fixed bg-[#00000066] top-0 z-50">
-            <div className={`h-[100%] w-[35%] bg-white ml-auto flex flex-col transition-transform duration-500 ease-in-out ${animationClass}`}>
+            <div className={`h-[100%] w-[35%] bg-white ml-auto flex flex-col transition-transform duration-500 ease-in-out ${animationClass}`} id="menu-container">
                 <div className="w-[100%] flex justify-between py-3 px-5 border-b border-gray-300 border-opacity-50">
                     <div className="flex gap-5">
                         <Image src={Avatar} className="rounded-full w-[50px]" alt="Avatar" />
@@ -53,19 +58,40 @@ export default function MenuBar({onClose, menuStatus}) {
 
                 <div>
                     <div className="w-[100%]">
+                        <button className="hidden w-[100%] flex justify-between px-5 py-3" onClick={() => setOpenNav(prev => !prev)} id="navigation-container">
+                            <KeyboardArrowDownIcon className={`text-[#424b4a] ${openNav ? "rotate-180" : undefined}`} />
+
+                            <div className="flex gap-3">
+                                <p className={`${workSans.className} text-normal font-normal text-[#424b4a] uppercase`}>Navigation</p>
+                                <NavigationOutlinedIcon className={`text-[#424b4a] rotate-270`} />
+                            </div>
+                        </button>
+
+                        {openNav ? <nav className="flex flex-col text-center">
+                                        <Link href="/home" className={`${workSans.className} font-normal text-normal text-[#424b4a] uppercase px-5 py-3 border-t border-gray-300 border-opacity-50`}>Home</Link>
+                                        <Link href="/home" className={`${workSans.className} font-normal text-normal text-[#424b4a] uppercase px-5 py-3`}>About</Link>
+                                        <Link href="/home" className={`${workSans.className} font-normal text-normal text-[#424b4a] uppercase px-5 py-3`}>Shop</Link>
+                                        <Link href="/home" className={`${workSans.className} font-normal text-normal text-[#424b4a] uppercase px-5 py-3 border-b border-gray-300 border-opacity-50`}>Contact</Link>
+                                    </nav> : undefined}
+
                         <Link href="/account" className="flex gap-3 font-medium text-[#424b4a] justify-end px-5 py-3 hover:bg-[#C25C5C] hover:text-white delay-75 duration-300">
-                            <p className="text-[20px]">Account</p>
+                            <p className={`text-normal ${workSans.className} font-normal uppercase`}>Account</p>
                             <AccountCircleOutlinedIcon className="my-auto" />
                         </Link>
 
+                        <Link href="/cart" className="hidden flex gap-3 font-medium text-[#424b4a] justify-end px-5 py-3 hover:bg-[#C25C5C] hover:text-white delay-75 duration-300" id="cart-nav">
+                            <p className={`text-normal ${workSans.className} font-normal uppercase`}>Cart</p>
+                            <ShoppingCartOutlinedIcon className="my-auto" />
+                        </Link>
+
                         <Link href="/" className="flex gap-3 justify-end font-medium text-[#424b4a] px-5 py-3 hover:bg-[#C25C5C] hover:text-white delay-75 duration-300">
-                            <p className="text-[20px]">Sign Out</p>
+                            <p className={`text-normal uppercase ${workSans.className} font-normal`}>Sign Out</p>
                             <PowerSettingsNewIcon className="my-auto"/>
                         </Link>
                     </div>
                 </div>
 
-                <div className="w-[100%] border-t border-gray-300 border-opacity-50">
+                <div className="w-[100%] border-t border-gray-300 border-opacity-50 cart-container">
                     <h2 className="mt-5 text-[25px] font-semibold text-[#C25C5C] text-center">Your Cart</h2>
 
                     <div className="w-[100%] max-h-[332px] overflow-y-auto">
@@ -93,7 +119,7 @@ export default function MenuBar({onClose, menuStatus}) {
                 </div>
 
                 <div className="mt-10 px-3 w-[100%]">
-                    <Link href="/cart" className={`text-base w-[100%] py-3 px-5 text-center border banner-p text-[#C25C5C] m-auto font-medium ${workSans.className} hover:bg-[#C25C5C] hover:text-white delay-75 duration-300 flex justify-center items-center`}>VIEW CART</Link>
+                    <Link href="/cart" className={`text-base w-[100%] py-3 px-5 text-center border banner-p text-[#C25C5C] m-auto font-medium ${workSans.className} hover:bg-[#C25C5C] hover:text-white delay-75 duration-300 flex justify-center items-center cart-container`}>VIEW CART</Link>
                 </div>
             </div>
         </section>
