d7a8ca90435ccbce1509d428c68c08d15c83748d
Lmao1232j <vinhtran23042004@gmail.com>
2025-07-07 10:34:16 +0700

    Finish responsive for menu

--- next-web/src/app/Menu/Menu.jsx
+++ next-web/src/app/Menu/Menu.jsx
@@ -41,7 +41,7 @@ export default function MenuBar({onClose, menuStatus}) {
             <div className={`h-[100%] w-[35%] bg-white ml-auto flex flex-col transition-transform duration-500 ease-in-out ${animationClass}`} id="menu-container">
                 <div className="w-[100%] flex justify-between py-3 px-5 border-b border-gray-300 border-opacity-50">
                     <div className="flex gap-5">
-                        <Image src={Avatar} className="rounded-full w-[50px]" alt="Avatar" />
+                        <Image src={Avatar} className="rounded-full w-[50px]" alt="Avatar" id="avatar" />
                         <p className={`my-auto ${workSans.className} font-medium text-[#424b4a]`}>Username</p>
                     </div>
 
@@ -58,34 +58,34 @@ export default function MenuBar({onClose, menuStatus}) {
 
                 <div>
                     <div className="w-[100%]">
-                        <button className="hidden w-[100%] flex justify-between px-5 py-3" onClick={() => setOpenNav(prev => !prev)} id="navigation-container">
+                        <button className="hidden w-[100%] flex justify-between px-5 py-3 navigation-container" onClick={() => setOpenNav(prev => !prev)}>
                             <KeyboardArrowDownIcon className={`text-[#424b4a] ${openNav ? "rotate-180" : undefined}`} />
 
                             <div className="flex gap-3">
-                                <p className={`${workSans.className} text-normal font-normal text-[#424b4a] uppercase`}>Navigation</p>
+                                <p className={`${workSans.className} text-normal font-normal text-[#424b4a] uppercase my-auto`}>Navigation</p>
                                 <NavigationOutlinedIcon className={`text-[#424b4a] rotate-270`} />
                             </div>
                         </button>
 
-                        {openNav ? <nav className="flex flex-col text-center">
-                                        <Link href="/home" className={`${workSans.className} font-normal text-normal text-[#424b4a] uppercase px-5 py-3 border-t border-gray-300 border-opacity-50`}>Home</Link>
-                                        <Link href="/home" className={`${workSans.className} font-normal text-normal text-[#424b4a] uppercase px-5 py-3`}>About</Link>
-                                        <Link href="/home" className={`${workSans.className} font-normal text-normal text-[#424b4a] uppercase px-5 py-3`}>Shop</Link>
-                                        <Link href="/home" className={`${workSans.className} font-normal text-normal text-[#424b4a] uppercase px-5 py-3 border-b border-gray-300 border-opacity-50`}>Contact</Link>
+                        {openNav ? <nav className="flex flex-col text-right navigation-container">
+                                        <Link href="/home" className={`${workSans.className} font-normal text-xs text-[#424b4a] uppercase px-5 py-3 border-t border-gray-300 border-opacity-50 my-auto`}>Home</Link>
+                                        <Link href="/home" className={`${workSans.className} font-normal text-xs text-[#424b4a] uppercase px-5 py-3 my-auto`}>About</Link>
+                                        <Link href="/home" className={`${workSans.className} font-normal text-xs text-[#424b4a] uppercase px-5 py-3 my-auto`}>Shop</Link>
+                                        <Link href="/home" className={`${workSans.className} font-normal text-xs text-[#424b4a] uppercase px-5 py-3 border-b border-gray-300 border-opacity-50 my-auto`}>Contact</Link>
                                     </nav> : undefined}
 
                         <Link href="/account" className="flex gap-3 font-medium text-[#424b4a] justify-end px-5 py-3 hover:bg-[#C25C5C] hover:text-white delay-75 duration-300">
-                            <p className={`text-normal ${workSans.className} font-normal uppercase`}>Account</p>
+                            <p className={`text-normal ${workSans.className} font-normal uppercase my-auto`}>Account</p>
                             <AccountCircleOutlinedIcon className="my-auto" />
                         </Link>
 
                         <Link href="/cart" className="hidden flex gap-3 font-medium text-[#424b4a] justify-end px-5 py-3 hover:bg-[#C25C5C] hover:text-white delay-75 duration-300" id="cart-nav">
-                            <p className={`text-normal ${workSans.className} font-normal uppercase`}>Cart</p>
+                            <p className={`text-normal ${workSans.className} font-normal uppercase my-auto`}>Cart</p>
                             <ShoppingCartOutlinedIcon className="my-auto" />
                         </Link>
 
                         <Link href="/" className="flex gap-3 justify-end font-medium text-[#424b4a] px-5 py-3 hover:bg-[#C25C5C] hover:text-white delay-75 duration-300">
-                            <p className={`text-normal uppercase ${workSans.className} font-normal`}>Sign Out</p>
+                            <p className={`text-normal uppercase ${workSans.className} font-normal my-auto`}>Sign Out</p>
                             <PowerSettingsNewIcon className="my-auto"/>
                         </Link>
                     </div>
@@ -95,6 +95,54 @@ export default function MenuBar({onClose, menuStatus}) {
                     <h2 className="mt-5 text-[25px] font-semibold text-[#C25C5C] text-center">Your Cart</h2>
 
                     <div className="w-[100%] max-h-[332px] overflow-y-auto">
+                        <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
+                            <Image src={Image7} alt="Image7" className="w-[70px]" />
+
+                            <div className="w-[100%] flex flex-col gap-3">
+                                <div className="flex justify-between">
+                                    <p className={`${workSans.className} text-[#C25C5C] font-medium`}>Ceramic Pot</p>
+                                    <CloseCircleOutlined />
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <Counter className="flex" />
+                                    <p className={`${workSans.className} text-[#424b4a] font-normal`}>$99</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
+                            <Image src={Image7} alt="Image7" className="w-[70px]" />
+
+                            <div className="w-[100%] flex flex-col gap-3">
+                                <div className="flex justify-between">
+                                    <p className={`${workSans.className} text-[#C25C5C] font-medium`}>Ceramic Pot</p>
+                                    <CloseCircleOutlined />
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <Counter className="flex" />
+                                    <p className={`${workSans.className} text-[#424b4a] font-normal`}>$99</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
+                            <Image src={Image7} alt="Image7" className="w-[70px]" />
+
+                            <div className="w-[100%] flex flex-col gap-3">
+                                <div className="flex justify-between">
+                                    <p className={`${workSans.className} text-[#C25C5C] font-medium`}>Ceramic Pot</p>
+                                    <CloseCircleOutlined />
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <Counter className="flex" />
+                                    <p className={`${workSans.className} text-[#424b4a] font-normal`}>$99</p>
+                                </div>
+                            </div>
+                        </div>
+
                         <div className="w-[100%] p-5 flex gap-3 border-b border-gray-300 border-opacity-50">
                             <Image src={Image7} alt="Image7" className="w-[70px]" />
 
