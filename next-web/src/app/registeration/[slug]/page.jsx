f382390e12bb169258f7606ca724de79bdc2ad5c
Lmao1232j <vinhtran23042004@gmail.com>
2025-07-09 14:47:49 +0700

    Add slug to registeration

---
+++ next-web/src/app/registeration/[slug]/page.jsx
@@ -0,0 +1,30 @@
+"use client"
+
+import SignIn from "../SignIn"
+import SignUp from "../SignUp"
+import Forgot from "../Forgot"
+import { useRouter, useParams } from "next/navigation"
+import { useEffect } from "react"
+
+export default function RegisterationPage() {
+    const params = useParams();
+    const router = useRouter();
+
+    useEffect(() => {
+        const validSlug = ["sign-in", "sign-up", "forgot"];
+
+        if(!validSlug.includes(params.slug)) {
+            router.push("/home");
+        }
+    }, [params.slug, router]);
+
+    return (
+        <section className="w-[30%] h-[90%] m-auto flex bg-white" id="registeration-section">
+                {
+                    params.slug === "sign-in" ? <SignIn /> :
+                    params.slug === "sign-up" ? <SignUp /> :
+                    params.slug === "forgot" ? <Forgot /> : undefined
+                }
+        </section>
+    );
+}
--- next-web/src/app/registeration/page.jsx
+++ next-web/src/app/registeration/page.jsx
@@ -8,13 +8,7 @@ import "./page.css"
 function Registeration() {
     return (
         <section className="w-[30%] h-[90%] m-auto flex bg-white" id="registeration-section">
-            {/* <div className="w-[50%] h-full">
-                <Image src={Image4} alt="Image-4" className="h-full w-full object-fill" />
-            </div> */}
-
-            {/* <SignIn /> */}
-            {/* <SignUp /> */}
-            <Forgot />
+            <SignIn />
         </section>
     );
 }
