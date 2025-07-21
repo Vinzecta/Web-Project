c6151c6c70eadf8c6630490beafeb65285eeaf20
Lmao1232j <vinhtran23042004@gmail.com>
2025-07-21 15:22:12 +0700

    Finish blank validation in sign up section

--- next-web/src/app/registeration/SignUp.jsx
+++ next-web/src/app/registeration/SignUp.jsx
@@ -7,6 +7,7 @@ import { Work_Sans } from "next/font/google";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import Error from "../error message/Error";
+import { AnimatePresence } from "framer-motion";
 
 const workSans = Work_Sans({
   subsets: ["latin"],
@@ -16,117 +17,120 @@ const workSans = Work_Sans({
 
 function SignUp() {
     const [formData, setFormData] = useState({first: "", last: "", email: "", password: "", confirm: ""});
-    const [error, setError] = useState({});
-    const emailRegex = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/;
-    const [touch, setTouch] = useState({first: false, last: false, email: false, password: false, confirm: false});
-    const [didMount, setDidMount] = useState(false);
+    const [required, setRequired] = useState({first: "", last: "", email: "", password: "", confirm: ""});
+    const [submit, setSubmit] = useState(false);
+
+    const changeBlankError = (form) => {
+        if (formData[form] !== "" && required[form] !== "") {
+            setRequired(prev => ({ ...prev, [form]: "" }));
+        }
+    }
 
     useEffect(() => {
-    setDidMount(true);
-    }, []);
+        for (const key in formData) {
+            changeBlankError(key);
+        }
+    }, [formData]);
 
-    useEffect(() => {
-        if (!didMount) {
-            return;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmit(true);
+
+        const blankInput = {first: "", last: "", email: "", password: "", confirm: ""};
+        if (formData.first.trim() === "") {
+            blankInput.first = "This field is required!";
         }
 
-        const formError = {first: "", last: "", email: [], password: [], confirm: []}
-        if (formData.first === "") {
-            formError.first = "First name is required!";
+        if (formData.last.trim() === "") {
+            blankInput.last = "This field is required!";
         }
 
-        if (formData.last === "") {
-            formError.last = "Last name is required!";
-        }
-
-        if (!emailRegex.test(formData.email)) {
-            formError.email.push("Email is invalid!");
-        }
-
-        if (formData.email === "") {
-            formError.email.push("This field is required!");
+        if (formData.email.trim() === "") {
+            blankInput.email = "This field is required!";
         }
 
         if (formData.password === "") {
-            formError.password.push("This field is required!");
+            blankInput.password = "This field is required!"
         }
 
         if (formData.confirm === "") {
-            formError.confirm.push("This field is required!");
+            blankInput.confirm = "This field is required!";
         }
 
-        if (formData.confirm != formData.password) {
-            formError.confirm.push("Password does not match!");
+        setRequired(blankInput);
+
+        for (const key in blankInput) {
+            if (blankInput[key] != "") {
+                return;
+            }
         }
 
-        setError(formError);
-    }, [formData])
-
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-        setFormData(prev => ({...prev, [name]: value}));
-    }
-
-    const handleTouch = (e) => {
-        const {name} = e.target;
-        setTouch(prev => ({...prev, [name]: true}));
-    }
+    };
 
     return (
-        <form className="flex flex-col gap-5 p-5 m-auto">
+        <form className="w-[100%] flex flex-col gap-5 p-5 m-auto" onSubmit={handleSubmit}>
             <h2 className="text-[25px] text-center text-[#C25C5C] font-medium">Sign Up</h2>
 
             <div className="flex justify-between">
-                <div className="w-[45%] flex flex-col gap-5">
-                    <label className={`text-base ${workSans.className} font-bold text-[#424b4a]`}>First Name <span className="text-[red]">*</span></label>
-                    <input name="first" onChange={handleChange} className={`bg-[white] h-[50px] p-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} text-[#424b4a] font-normal`}></input>
-                </div>
-
-                <div className="w-[45%] flex flex-col gap-5">
-                    <label className={`text-base ${workSans.className} font-bold text-[#424b4a]`}>Last Name <span className="text-[red]">*</span></label>
-                    <input name="last" onChange={handleChange} className="bg-[white] h-[50px] p-3 border border-gray-300 border-opacity-50 w-[100%]"></input>
-                </div>
+                <label className={`text-base ${workSans.className} font-bold text-[#424b4a]`}>First Name <span className="text-[red]">*</span></label>
+                <AnimatePresence mode="wait">
+                    {
+                        (required.first != "" && submit) ? <Error error={required.first} /> : undefined
+                    }
+                </AnimatePresence>
             </div>
+            <input onInput={(e) => setFormData({...formData, first: e.target.value})} name="first" className={`bg-[white] h-[50px] p-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} text-[#424b4a] font-normal`}></input>
 
-            {
-                touch.first && error.first.length > 0 ? <Error error={error.first} /> : undefined
-            }
+            <div className="flex justify-between">
+                <label className={`text-base ${workSans.className} font-bold text-[#424b4a]`}>Last Name <span className="text-[red]">*</span></label>
+                <AnimatePresence mode="wait">
+                    {
+                        (required.last != "" && submit) ? <Error error={required.last} /> : undefined
+                    }
+                </AnimatePresence>
+            </div>
+            <input onInput={(e) => setFormData({...formData, last: e.target.value})} name="last" className={`bg-[white] h-[50px] p-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} text-[#424b4a] font-normal`}></input>
 
-            {
-                touch.last && error.last.length > 0 ? <Error error={error.last} /> : undefined
-            }
+            <div className="flex justify-between">
+                <label className={`text-base ${workSans.className} font-bold text-[#424b4a]`}>Email <span className="text-[red]">*</span></label>
+                <AnimatePresence mode="wait">
+                    {
+                        (required.email != "" && submit) ? <Error error={required.email} /> : undefined
+                    }
+                </AnimatePresence>
+            </div>
+            <input onInput={(e) => setFormData({...formData, email: e.target.value})} name="email" className={`bg-[white] h-[50px] p-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} text-[#424b4a] font-normal`}></input>
 
-            <label className={`text-base ${workSans.className} font-bold text-[#424b4a]`}>Email <span className="text-[red]">*</span></label>
-            <input name="email" onChange={handleChange} className="bg-[white] h-[50px] p-3 border border-gray-300 border-opacity-50 w-[100%]"></input>
+            <div className="flex justify-between">
+                <label className={`text-base ${workSans.className} font-bold text-[#424b4a]`}>Password <span className="text-[red]">*</span></label>
+                <AnimatePresence mode="wait">
+                    {
+                        (required.password != "" && submit) ? <Error error={required.password} /> : undefined
+                    }
+                </AnimatePresence>
+            </div>
+            <input onInput={(e) => setFormData({...formData, password: e.target.value})} name="password" className={`bg-[white] h-[50px] p-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} text-[#424b4a] font-normal`}></input>
 
-            {
-                touch.email && error.email.length > 0 ? error.email.map((errors, index) => <Error key={index} error={errors} />) : undefined
-            }
-
-            <label className={`text-base ${workSans.className} font-bold text-[#424b4a]`}>Password <span className="text-[red]">*</span></label>
-            <input name="password" onChange={handleChange} className="bg-[white] h-[50px] p-3 border border-gray-300 border-opacity-50 w-[100%]"></input>
-
-            {
-                touch.password && error.password.length > 0 ? error.password.map((errors, index) => <Error key={index} error={errors} />) : undefined
-            }
-
-            <label className={`text-base ${workSans.className} font-bold text-[#424b4a]`}>Confirm Password <span className="text-[red]">*</span></label>
-            <input name="confirm" onChange={handleChange} className="bg-[white] h-[50px] p-3 border border-gray-300 border-opacity-50 w-[100%]"></input>
-
-            {
-                touch.confirm && error.confirm.length > 0 ? error.confirm.map((errors, index) => <Error key={index} error={errors} />) : undefined
-            }
+            <div className="flex justify-between">
+                <label className={`text-base ${workSans.className} font-bold text-[#424b4a]`}>Confirm Password <span className="text-[red]">*</span></label>
+                <AnimatePresence mode="wait">
+                    {
+                        (required.confirm != "" && submit) ? <Error error={required.confirm} /> : undefined
+                    }
+                </AnimatePresence>
+            </div>
+            <input onInput={(e) => setFormData({...formData, confirm: e.target.value})} name="confirm" className={`bg-[white] h-[50px] p-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} text-[#424b4a] font-normal`}></input>
 
             <button className={`text-base w-1/1 py-3 px-5 text-center border banner-p text-[#C25C5C] rounded-3xl mx-auto border-[#C25C5C] font-medium ${workSans.className} hover:bg-[#C25C5C] hover:text-white delay-75 duration-300 flex justify-center items-center`}>Sign Up</button>
 
             <p className={`text-base text-center ${workSans.className} font-normal text-[#424b4a]`}>Or</p>
 
-            <div className="flex gap-5 justify-center bg-[white] h-[50px] py-3 border border-gray-300 border-opacity-50 w-[100%] rounded-3xl">
+            <div className="flex gap-5 justify-center bg-[white] h-[50px] py-3 border border-gray-300 border-opacity-50 w-[100%] rounded-3xl hover:scale-105 cursor-pointer transition-transform duration-300">
                 <Image src={Facebook} alt="Facebook" className="w-[30px]" />
                 <p className={`text-base my-auto ${workSans.className} font-medium text-[#424b4a]`}>Sign Up with Facebook</p>
             </div>
 
-            <div className="flex gap-5 justify-center bg-[white] h-[50px] py-3 border border-gray-300 border-opacity-50 w-[100%] rounded-3xl">
+            <div className="flex gap-5 justify-center bg-[white] h-[50px] py-3 border border-gray-300 border-opacity-50 w-[100%] rounded-3xl hover:scale-105 cursor-pointer transition-transform duration-300">
                 <Image src={Google} alt="Google" className="w-[30px]" />
                 <p className={`text-base my-auto ${workSans.className} font-medium text-[#424b4a]`}>Sign Up with Google</p>
             </div>
