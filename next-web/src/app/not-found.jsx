cf7292d7498ff083b2f949e88fe5ebed77c7184a
Lmao1232j <vinhtran23042004@gmail.com>
2025-07-22 15:35:30 +0700

    Finish responsive for not found page

--- next-web/src/app/not-found.jsx
+++ next-web/src/app/not-found.jsx
@@ -12,17 +12,14 @@ const workSans = Work_Sans({
 
 export default function NotFound() {
     return (
-        <>
-            <Header />
-            <section className="flex justify-center w-[80%] h-screen mx-auto gap-10">
-                <Image src={Sad} alt="Sad" className="w-[20%] " />
-                <div className="flex flex-col gap-5 my-auto w-[50%]">
+        <section className="h-screen w-full">
+            <div className="flex justify-center w-[80%] h-screen mx-auto gap-10" id="not-found-section">
+                <div className="flex flex-col gap-5 my-auto w-[50%]" id="content-container">
                     <h1 className="text-[50px] font-medium text-[#C25C5C] w-[100%]">PAGE NOT FOUND!</h1>
                     <p className={`text-base font-normal text-[#424b4a] w-[100%] ${workSans.className}`}>The Requested URL Was Not Found!</p>
                     <p className={`text-base font-normal text-[#424b4a] w-[100%] ${workSans.className}`}>The page you are looking is not exist, have been removed, name changed or is temporarily unavailable.</p>
                 </div>
-            </section>
-            <Footer />
-        </>
+            </div>
+        </section>
     );
 }
