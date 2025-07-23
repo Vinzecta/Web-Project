0e9e162dc2d59166bf165a112adb0d3c974bae87
Lmao1232j <vinhtran23042004@gmail.com>
2025-07-23 17:16:57 +0700

    f

--- next-web/src/app/account/Profile.jsx
+++ next-web/src/app/account/Profile.jsx
@@ -1,10 +1,15 @@
 // Profile Section
+"use client"
+
 import Avatar from "../../../public/avatar.jpg"
 import Image from "next/image";
 import Information from "../../../public/information.svg"
 import Address from "../../../public/address.svg"
 import "./Profile.css"
 import { Work_Sans } from "next/font/google";
+import Error from "../error message/Error";
+import { useState, useEffect } from "react";
+import { AnimatePresence } from "framer-motion";
 
 const workSans = Work_Sans({
   subsets: ["latin"],
@@ -13,8 +18,78 @@ const workSans = Work_Sans({
 });
 
 function Profile() {
+    const [formData, setFormData] = useState({
+        first: "",
+        last: "",
+        email: "",
+        address: {
+            city: "",
+            province: "",
+            district: "",
+            ward: "",
+            street: "",
+        }
+    })
+    const [blank, setBlank] = useState({
+        first: "",
+        last: "",
+        email: "",
+        address: "",
+    })
+    const [submit, setSubmit] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmit(true);
+        
+        const blankInput = {
+            first: "",
+            last: "",
+            email: "",
+        }
+        const { first, last, email } = formData;
+        const inputForm = { first, last, email };
+
+        for (const key in blankInput) {
+            if (inputForm[key] === "") {
+                blankInput[key] = "Required!";
+            }
+        }
+
+        setBlank(blankInput);
+
+        if (formData.address.street === "") {
+            setBlank({...blankInput, address: "Required!"});
+        }
+
+        for (const key in blankInput) {
+            if (blankInput[key] !== "") {
+                return;
+            }
+        }
+    }
+
+    useEffect(() => {
+        for (const key in blank) {
+            if (key === "address") {
+                break;
+            }
+
+            if (blank[key] !== "" && formData[key] !== "") {
+                setBlank({
+                    ...blank,
+                    [key]: "",
+                })
+            }
+        }
+
+        if (blank.address !== "" && formData.address.street !== "") {
+            setBlank({...blank, address: ""});
+        }
+    }, [formData]);
+
     return (
-        <form className="w-[60%] mt-5 p-5 flex flex-col gap-5 rounded-2xl border border-gray-300 border-opacity-50 mb-10" id="profile-form">
+        <form onSubmit={handleSubmit} className="w-[60%] mt-5 p-5 flex flex-col gap-5 rounded-2xl border border-gray-300 border-opacity-50 mb-10" id="profile-form">
             <h2 className="text-[25px] pb-3 border-b border-black font-medium text-[#C25C5C]" id="profile-information">Profile Information</h2>
 
             {/* Avatar */}
@@ -32,25 +107,47 @@ function Profile() {
 
                 <div className="flex justify-between basic-info">
                     <div className="w-[45%] flex flex-col gap-2 flex-info">
-                        <label className={`${workSans.className} text-[#424b4a] font-bold`}>First Name <span className="text-[red]">*</span></label>
-                        <input className={`bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} font-normal`}></input>
+                        <div className="flex justify-between">
+                            <label className={`${workSans.className} text-[#424b4a] font-bold`}>First Name <span className="text-[red]">*</span></label>
+                            <AnimatePresence mode="wait">
+                                {
+                                    blank.first != "" && submit ? 
+                                    <Error error={blank.first} /> :
+                                    undefined
+                                }
+                            </AnimatePresence>
+                        </div>
+                        <input onInput={(e) => setFormData({...formData, first: e.target.value})} className={`bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} font-normal`}></input>
                     </div>
 
                     <div className="w-[45%] flex flex-col gap-2 flex-info">
-                        <label className={`${workSans.className} text-[#424b4a] font-bold`}>Last Name <span className="text-[red]">*</span></label>
-                        <input className={`bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} font-normal`}></input>
+                        <div className="flex justify-between">
+                            <label className={`${workSans.className} text-[#424b4a] font-bold`}>Last Name <span className="text-[red]">*</span></label>
+                            <AnimatePresence mode="wait">
+                                {
+                                    blank.last !== "" && submit ? 
+                                    <Error error={blank.last} /> :
+                                    undefined
+                                }
+                            </AnimatePresence>
+                        </div>
+                        <input onInput={(e) => setFormData({...formData, last: e.target.value})} className={`bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} font-normal`}></input>
                     </div>
                 </div>
 
                 <div className="flex justify-between basic-info">
-                    <div className="w-[45%] flex flex-col gap-2 flex-info">
-                        <label className={`${workSans.className} text-[#424b4a] font-bold`}>Username <span className="text-[red]">*</span></label>
-                        <input className={`bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} font-normal`}></input>
-                    </div>
-
-                    <div className="w-[45%] flex flex-col gap-2 flex-info">
-                        <label className={`${workSans.className} text-[#424b4a] font-bold`}>Email <span className="text-[red]">*</span></label>
-                        <input className={`bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} font-normal`}></input>
+                    <div className="w-[100%] flex flex-col gap-2 flex-info">
+                        <div className="flex justify-between">
+                            <label className={`${workSans.className} text-[#424b4a] font-bold`}>Email <span className="text-[red]">*</span></label>
+                            <AnimatePresence mode="wait">
+                                {
+                                    blank.email !== "" && submit ?
+                                    <Error error={blank.email} /> :
+                                    undefined
+                                }
+                            </AnimatePresence>
+                        </div>
+                        <input onInput={(e) => setFormData({...formData, email: e.target.value})} className={`bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} font-normal`}></input>
                     </div>
                 </div>
             </div>
@@ -85,8 +182,17 @@ function Profile() {
                 </div>
 
                 <div className="flex flex-col gap-2">
-                    <label className={`${workSans.className} text-[#424b4a] font-bold`}>Street Address <span className="text-[red]">*</span></label>
-                    <input type="text" className={`bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} font-normal`}></input>
+                    <div className="flex justify-between">
+                        <label className={`${workSans.className} text-[#424b4a] font-bold`}>Street Address <span className="text-[red]">*</span></label>
+                        <AnimatePresence mode="wait">
+                            {
+                                blank.address !== "" && submit ?
+                                <Error error={blank.address} /> :
+                                undefined
+                            }
+                        </AnimatePresence>
+                    </div>
+                    <input onInput={(e) => setFormData({...formData, address: {...formData.address, street: e.target.value}})} type="text" className={`bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} font-normal`}></input>
                 </div>
 
                 <button className={`text-base w-fit py-3 px-5 text-center border banner-p text-[#C25C5C] rounded-3xl mx-auto border-[#C25C5C] font-medium ${workSans.className} hover:bg-[#C25C5C] hover:text-white delay-75 duration-300 flex justify-center items-center`}>Save Changes</button>
--- next-web/src/app/account/ResetPassword.jsx
+++ next-web/src/app/account/ResetPassword.jsx
@@ -1,5 +1,12 @@
+"use client"
+
 import "./ResetPassword.css"
 import { Work_Sans } from "next/font/google";
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
+import { useState, useEffect } from "react";
+import Error from "../error message/Error";
+import { AnimatePresence } from "framer-motion";
 
 const workSans = Work_Sans({
   subsets: ["latin"],
@@ -8,24 +15,136 @@ const workSans = Work_Sans({
 });
 
 function ResetPassword() {
+    const [formData, setFormData] = useState({
+        current: "",
+        new: "",
+        confirm: "",
+    });
+
+    const [blank, setBlank] = useState({
+        current: "",
+        new: "",
+        confirm: "",
+    });
+
+    const [submit, setSubmit] = useState(false);
+    const [typing, setTyping] = useState({
+        current: formData.current.length > 0,
+        new: formData.new.length > 0,
+        confirm: formData.confirm.length > 0
+    });
+
+    const [seePassword, setSeePassword] = useState({
+        current: false,
+        new: false,
+        confirm: false
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmit(true);
+
+        const blankError = {
+            current: "",
+            new: "",
+            confirm: "",
+        }
+
+        for (const key in formData) {
+            if (formData[key] === "") {
+                blankError[key] = "Required!"
+            }
+        }
+
+        setBlank(blankError);
+    }
+
+    useEffect(() => {
+        for (const key in formData) {
+            if (blank[key] !== "" && formData[key] !== "") {
+                setBlank({...blank, [key]: ""})
+            }
+        }
+
+        setTyping({
+            current: formData.current.length > 0,
+            new: formData.new.length > 0,
+            confirm: formData.confirm.length > 0
+        })
+    }, [formData]);
+
     return (
-        <form className="w-[60%] mt-5 p-5 flex flex-col gap-5 rounded-2xl border border-gray-300 border-opacity-50 mb-10">
+        <form onSubmit={handleSubmit} className="w-[60%] mt-5 p-5 flex flex-col gap-5 rounded-2xl border border-gray-300 border-opacity-50 mb-10">
             <h2 className="text-[25px] pb-3 border-b border-black text-[#C25C5C] font-medium">Reset Password</h2>
 
             <div className="flex flex-col gap-5">
                 <div className="flex flex-col gap-2">
-                    <label className={`${workSans.className} text-[#424b4a] font-bold`}>Current Password <span className="text-[red]">*</span></label>
-                    <input className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%]"></input>
+                    <div className="flex justify-between">
+                        <label className={`${workSans.className} text-[#424b4a] font-bold`}>Current Password <span className="text-[red]">*</span></label>
+                        <AnimatePresence mode="wait">
+                            {
+                                blank.current !== "" && submit ? 
+                                <Error error={blank.current} />:
+                                undefined
+                            }
+                        </AnimatePresence>
+                    </div>
+                    <div className="relative w-full">
+                        <input type={seePassword.current ? "text" : "password"} onInput={(e) => setFormData({...formData, current: e.target.value})} className={`bg-[white] h-[50px] px-3 pr-10 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} font-normal text-[#424b4a]`} />
+                        {
+                            typing.current ? 
+                            seePassword.current ?
+                            <VisibilityOutlinedIcon onClick={() => setSeePassword((prev) => ({...prev, current: !prev.current}))} className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer" sx={{color: "#999999", fontWeight: "light"}} /> :
+                            <VisibilityOffOutlinedIcon onClick={() => setSeePassword((prev) => ({...prev, current: !prev.current}))} className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer" sx={{color: "#999999", fontWeight: "light"}} /> :
+                            undefined
+                        }
+                    </div>
                 </div>
 
                 <div className="flex flex-col gap-2">
-                    <label className={`${workSans.className} text-[#424b4a] font-bold`}>New Password <span className="text-[red]">*</span></label>
-                    <input className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%]"></input>
+                    <div className="flex justify-between">
+                        <label className={`${workSans.className} text-[#424b4a] font-bold`}>New Password <span className="text-[red]">*</span></label>
+                        <AnimatePresence mode="wait">
+                            {
+                                blank.new !== "" && submit ? 
+                                <Error error={blank.new} /> :
+                                undefined
+                            }
+                        </AnimatePresence>
+                    </div>
+                    <div className="relative w-full">
+                        <input type={seePassword.new ? "text" : "password"} onInput={(e) => setFormData({...formData, new: e.target.value})} className={`bg-[white] h-[50px] px-3 pr-10 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} font-normal text-[#424b4a]`} />
+                        {
+                            typing.new ? 
+                            seePassword.new ?
+                            <VisibilityOutlinedIcon onClick={() => setSeePassword((prev) => ({...prev, new: !prev.new}))} className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer" sx={{color: "#999999", fontWeight: "light"}} /> :
+                            <VisibilityOffOutlinedIcon onClick={() => setSeePassword((prev) => ({...prev, new: !prev.new}))} className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer" sx={{color: "#999999", fontWeight: "light"}} /> :
+                            undefined
+                        }
+                    </div>
                 </div>
 
                 <div className="flex flex-col gap-2">
-                    <label className={`${workSans.className} text-[#424b4a] font-bold`}>Confirm Password <span className="text-[red]">*</span></label>
-                    <input className="bg-[white] h-[50px] px-3 border border-gray-300 border-opacity-50 w-[100%]"></input>
+                    <div className="flex justify-between">
+                        <label className={`${workSans.className} text-[#424b4a] font-bold`}>Confirm Password <span className="text-[red]">*</span></label>
+                        <AnimatePresence mode="wait">
+                            {
+                                blank.confirm !== "" && submit ? 
+                                <Error error={blank.confirm} /> :
+                                undefined
+                            }
+                        </AnimatePresence>
+                    </div>
+                    <div className="relative w-full">
+                        <input type={seePassword.confirm ? "text" : "password"} onInput={(e) => setFormData({...formData, confirm: e.target.value})} className={`bg-[white] h-[50px] px-3 pr-10 border border-gray-300 border-opacity-50 w-[100%] ${workSans.className} font-normal text-[#424b4a]`} />
+                        {
+                            typing.confirm ? 
+                            seePassword.confirm ?
+                            <VisibilityOutlinedIcon onClick={() => setSeePassword((prev) => ({...prev, confirm: !prev.confirm}))} className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer" sx={{color: "#999999", fontWeight: "light"}} /> :
+                            <VisibilityOffOutlinedIcon onClick={() => setSeePassword((prev) => ({...prev, confirm: !prev.confirm}))} className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer" sx={{color: "#999999", fontWeight: "light"}} /> :
+                            undefined
+                        }
+                    </div>
                 </div>
             </div>
 
